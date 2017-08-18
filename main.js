69c6b6d384d97948c82fc4a5ab1e352a2bff81ef
blinklv <blinklv@icloud.com>
2017-08-18 12:41:46 +0800

    Add header.

---
+++ main.js
@@ -0,0 +1,14 @@
+// main.js
+//
+// Author: blinklv <blinklv@icloud.com>
+// Create Time: 2017-08-18
+// Maintainer: blinklv <blinklv@icloud.com>
+// Last Change: 2017-08-18
+
+(function() {
+  hljs.initHighlightingOnLoad();
+})();
+
+
+
+
